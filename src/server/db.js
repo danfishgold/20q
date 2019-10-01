f8830e1932b2f3e08ef0bd3a13b0170b8610e585
Dan Fishgold <d.fishgold@gmail.com>
2019-10-02 01:55:19 +0300

    FAUNADB IS ANNOYING

--- src/server/db.js
+++ src/server/db.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
-import { query as q, Client } from 'faunadb'
+// require('dotenv').config()
+// import { query as q, Client } from 'faunadb'
 
-const client = new Client({ secret: process.env.FAUNADB_SECRET_KEY })
+// const client = new Client({ secret: process.env.FAUNADB_SECRET_KEY })
 
 export async function fetch_quiz(quiz_id) {
-  try {
-    const result = await client.query(
-      q.Get(q.Match(q.Index('quizes_by_id'), quiz_id)),
-    )
-    return result.data
-  } catch {
-    return {}
-  }
+  // try {
+  //   const result = await client.query(
+  //     q.Get(q.Match(q.Index('quizes_by_id'), quiz_id)),
+  //   )
+  //   return result.data
+  // } catch {
+  //   return {}
+  // }
+  return {}
 }
 
 export async function save_quiz(quiz) {
-  return await client.query(q.Create(q.Collection('quizes'), { data: quiz }))
+  // return await client.query(q.Create(q.Collection('quizes'), { data: quiz }))
 }
--- src/server/functions/quiz_by_id.js
+++ src/server/functions/quiz_by_id.js
@@ -4,7 +4,11 @@ import * as util from '../util'
 
 export async function fetch_quiz(quiz_id) {
   const db_quiz = await db.fetch_quiz(quiz_id)
-  if (!util.quiz_has_metadata(db_quiz) || !util.quiz_has_items(db_quiz)) {
+  if (
+    !db_quiz ||
+    !util.quiz_has_metadata(db_quiz) ||
+    !util.quiz_has_items(db_quiz)
+  ) {
     const haaretz_quiz = await haaretz.fetch_quiz(quiz_id)
     await db.save_quiz(haaretz_quiz)
     return haaretz_quiz
