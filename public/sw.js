63171755e87c6b9bac45a353ccbf1aaa3bb49685
Dan Fishgold <d.fishgold@gmail.com>
2020-05-24 21:26:44 +0300

    Make the service worker "network falling back to the cache"
    (Also some Prettier formatting)
    https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#network_falling_back_to_the_cache

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,24 @@
 const cacheName = '20q'
 const filesToCache = ['/', '/index.html', '/elm.js']
-self.addEventListener('install', e => {
+
+self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install')
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
       console.log('[ServiceWorker] Caching app shell')
       return cache.addAll(filesToCache)
-    }),
+    })
   )
 })
-self.addEventListener('activate', event => {
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim())
 })
-self.addEventListener('fetch', event => {
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then(response => {
-      return response || fetch(event.request)
-    }),
+    fetch(event.request).catch(() => {
+      return caches.match(event.request, { ignoreSearch: true })
+    })
   )
 })
